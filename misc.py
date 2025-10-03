57667111a4d7e5c475446d9a7a0fe634c347f322
Ashutosh Tripathi <ashutoshtrirpform@gmail.com>
2025-10-03 18:06:47 +0530

    refactor(misc): make functions generic (train_model, predict, etc.)

--- misc.py
+++ misc.py
@@ -1,11 +1,10 @@
-from typing import Tuple
+from typing import Tuple, Any
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
 def load_data() -> pd.DataFrame:
-
     data_url = "http://lib.stat.cmu.edu/datasets/boston"
     raw_df = pd.read_csv(data_url, sep=r"\s+", skiprows=22, header=None)
     data = np.hstack([raw_df.values[::2, :], raw_df.values[1::2, :2]])
@@ -26,6 +25,13 @@ def split_features_targets(df: pd.DataFrame, target_col: str = 'MEDV') -> Tuple[
 def train_test_split_data(X: np.ndarray, y: np.ndarray, test_size: float = 0.2, random_state: int = 42):
     return train_test_split(X, y, test_size=test_size, random_state=random_state)
 
-def evaluate_mse(model, X_test: np.ndarray, y_test: np.ndarray) -> float:
-    preds = model.predict(X_test)
+def train_model(model: Any, X_train: np.ndarray, y_train: np.ndarray) -> Any:
+    model.fit(X_train, y_train)
+    return model
+
+def predict(model: Any, X_test: np.ndarray) -> np.ndarray:
+    return model.predict(X_test)
+
+def evaluate_mse(model: Any, X_test: np.ndarray, y_test: np.ndarray) -> float:
+    preds = predict(model, X_test)
     return mean_squared_error(y_test, preds)
